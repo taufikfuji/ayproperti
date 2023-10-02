25fba7c49bd12f8e62ae33707f930c7d7085dfe2
taufikfuji <fujitaufik@gmail.com>
2023-10-02 16:51:07 +0700

    update

--- server/index.js
+++ server/index.js
@@ -1,17 +1,17 @@
-// const express = require ('express')
-// const app = express ()
-// const port = 3000
-// const cors = require('cors')
+const express = require ('express')
+const app = express ()
+const port = 3000
+const cors = require('cors')
 
-// app.use(cors())
-// app.get('/get-datas',(req,res)=>{
-//     res.status(200).json({
-//         "name" : "fuji taufik",
-//         "email" : "fujitaufik@gmail.com",
-//         "phone_number" : "08121989217"
-//     })
-// })
+app.use(cors())
+app.get('/get-datas',(req,res)=>{
+    res.status(200).json({
+        "name" : "fuji taufik",
+        "email" : "fujitaufik@gmail.com",
+        "phone_number" : "08121989217"
+    })
+})
 
-// app.listen(port, () => {
-//     console.log(`Example app listeng on port ${port}`);
-// })
+app.listen(port, () => {
+    console.log(`Example app listeng on port ${port}`);
+})
